150bd01182d0af97bd310ed9bc9b00c06f418e22
kharismarizqii <rizqikharisma@gmail.com>
2021-02-19 18:09:33 +0700

    layout

--- app/src/main/java/com/kharismarizqii/githubuserapp/MainActivity.kt
+++ app/src/main/java/com/kharismarizqii/githubuserapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.kharismarizqii.githubuserapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.kharismarizqii.githubuserapp.R
+import com.kharismarizqii.githubuserapp.databinding.AppBarBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: AppBarBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = AppBarBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_app_bar, menu)
+        return true
     }
 }
